63d9aaf2a26da3148ce7e6546e397212b463244e
Antti Holvikari <antti@anttih.com>
2023-11-11 09:37:15 +0200

    Restructure benchmarks module

--- bench/Main.purs
+++ bench/Main.purs
@@ -2,13 +2,52 @@ module Bench.Main where
 
 import Prelude
 
-import Data.ByteString (ByteString, CodePoint)
+import Data.ByteString (CodePoint)
 import Data.ByteString as BS
 import Data.Function.Uncurried (Fn2, Fn3, runFn2)
 import Data.Maybe (Maybe)
 import Effect (Effect)
 import Effect.Console (log)
 import Performance.Minibench (benchWith)
+
+main :: Effect Unit
+main = do
+  input <- readTextFile "./alice.txt"
+  inputUnicode <- readTextFile "./alice.unicode.txt"
+
+  inputBs <- pure (BS.fromString input)
+  inputBsUnicode <- pure (BS.fromString inputUnicode)
+
+  log "native string-length ascii" 
+  benchWith 100 \_ -> lengthNative input
+  log "---"
+
+  log "native string-length unicode" 
+  benchWith 100 \_ -> lengthNative inputUnicode
+  log "---"
+
+  log "ByteString length (code units) ascii"
+  benchWith 100 \_ -> BS.length inputBs
+  log "---"
+
+  log "ByteString length (code units) unicode"
+  benchWith 100 \_ -> BS.length inputBsUnicode
+  log "---"
+
+  log "ByteString length (code points) ascii"
+  benchWith 100 \_ -> BS.lengthCodePoints inputBs
+  log "---"
+
+  log "ByteString length (code points) unicode"
+  benchWith 100 \_ -> BS.lengthCodePoints inputBsUnicode
+  log "---"
+
+  log "native string-append (concat)"
+  benchWith 100 \_ -> runFn2 stringAppend inputUnicode inputUnicode
+  log "---"
+  
+  log "ByteString concat"
+  benchWith 100 \_ -> inputBsUnicode <> inputBsUnicode
 
 foreign import unconsCodePointImpl ::
   Fn3
@@ -22,64 +61,4 @@ foreign import readTextFile :: String -> Effect String
 foreign import lengthNative :: String -> Int
 
 foreign import stringAppend :: Fn2 String String String
-
-main :: Effect Unit
-main = do
-  input <- readTextFile "./alice.txt"
-  inputUnicode <- readTextFile "./alice.unicode.txt"
-
-  inputBs <- pure (BS.fromString input)
-  inputBsUnicode <- pure (BS.fromString inputUnicode)
-
-  benchNativeStringLength input inputUnicode
-
-  benchCodeUnits inputBs inputBsUnicode
-  benchCodePoints inputBs inputBsUnicode
-
-  benchConcatNative inputUnicode
-  benchConcat inputBsUnicode
-
-  where
-  
-  benchNativeStringLength :: String -> String -> Effect Unit
-  benchNativeStringLength ascii unicode = do
-    log "---"
-    log "native string-length ascii" 
-    benchWith 100 \_ -> lengthNative ascii
-
-    log "---"
-    log "native string-length unicode" 
-    benchWith 100 \_ -> lengthNative unicode
-
-  benchCodeUnits :: ByteString -> ByteString -> Effect Unit
-  benchCodeUnits ascii unicode = do
-    log "---"
-    log "ByteString length (code units) ascii"
-    benchWith 100 \_ -> BS.length ascii
-
-    log "---"
-    log "ByteString length (code units) unicode"
-    benchWith 100 \_ -> BS.length unicode
-
-  benchCodePoints :: ByteString -> ByteString -> Effect Unit
-  benchCodePoints ascii unicode = do
-    log "---"
-    log "ByteString length (code points) ascii"
-    benchWith 100 \_ -> BS.lengthCodePoints ascii
-
-    log "---"
-    log "ByteString length (code points) unicode"
-    benchWith 100 \_ -> BS.lengthCodePoints unicode
-
-  benchConcatNative :: String -> Effect Unit
-  benchConcatNative unicode = do
-    log "---"
-    log "native string-append (concat)"
-    benchWith 100 \_ -> runFn2 stringAppend unicode unicode
-
-  benchConcat :: ByteString -> Effect Unit
-  benchConcat unicode = do
-    log "---"
-    log "ByteString concat"
-    benchWith 100 \_ -> unicode <> unicode
 
